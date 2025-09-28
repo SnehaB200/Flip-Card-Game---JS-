e40ec215c0924f19dec886832fae8a5ab8ff1072
Snehangi Behera <115468014+SnehaB200@users.noreply.github.com>
2025-09-28 20:30:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+<h2>#Memory Flip Card Game</h2>
+<br>
+<p>A classic card-matching memory game built to test and improve your memory skills. Match all the pairs to win!
+</p>
+<br>
+<h3> Features</h3>
+
+<p> 1. Classic Gameplay: Flip cards to find matching pairs. <br></p>
+<p> 2. Responsive Design: Playable on desktops, tablets, and mobile phones.</p>
+<p> 3. Move Counter: Tracks the number of moves you've made. <br></p>
+<p> 4. Timer: See how quickly you can complete the game. <br></p>
+<p> 5. Refresh Game: A button to easily shuffle and start a new game. <br></p>
+<h3> How to Play</h3>
+<br>
+<p>1. Start the Game: Open the index.html file in your web browser. <br></p>
+<p>2. Flip a Card: Click on any card to reveal its face. <br></p>
+<p>3. Find the Match: Click on a second card to see if it's a match. <br></p>
+<p>4. If it's a Match: The cards will remain face-up. <br></p>
+<p>5. If it's Not a Match: The cards will flip back over after a short delay. <br></p>
+<p>6. Win the Game: The game is won when you have successfully matched all the pairs on the board. <br></p>
+<p>7. Play Again: Click the "Restart" button to shuffle the cards and start a new game. <br></p>
---
+++ main.js
@@ -0,0 +1,94 @@
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
+
+let maxTime  = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
+let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
+
+function initTimer() {
+    if(timeLeft <= 0){
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+}
+
+function flipCard({target: clickedCard}) {
+    if(!isPlaying){
+        isPlaying = true;
+        timer = setInterval(initTimer, 1000);
+    }
+    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0){
+        flips++;
+        flipsTag.innerText = flips;
+        clickedCard.classList.add("flip");
+        if(!cardOne){
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneIcon = cardOne.querySelector(".back-view i").classList.value;
+        let cardTwoIcon = cardTwo.querySelector(".front-view i").classList.value;
+        matchCards(cardOneIcon, cardTwoIcon);
+    }
+}
+
+function matchCards(icon1, icon2) {
+    if(icon1 === icon2){
+        matchedCard++;
+        if(matchedCard == 6 && timeLeft > 0){
+            return clearInterval(timer);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+
+    setTimeout(() =>{
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() =>{
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCards() {
+    timeLeft = maxTime;
+    flips = matchCards = 0;
+    cardOne = cardTwo = "";
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
+
+    let arr = ["fa-instagram", "fa-linkedin-in", "fa-whatsapp", "fa-youtube", "fa-twitter", "fa-facebook","fa-instagram", "fa-linkedin-in", "fa-whatsapp", "fa-youtube", "fa-twitter", "fa-facebook"];
+    arr.sort(() =>Math.random() > 0.5 ? 1 : -1);
+
+    cards.forEach((card, index) =>{
+        card.classList.remove("flip");
+        let iconTag = card.querySelector(".back-view i");
+        setTimeout(()=>{
+            iconTag.classList.value = `fa-${arr[index]}`;
+        }, 500);
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCards();
+
+refreshBtn.addEventListener("click", shuffleCards);
+cards.forEach(card =>{
+    card.addEventListener("click", flipCard);
+});
